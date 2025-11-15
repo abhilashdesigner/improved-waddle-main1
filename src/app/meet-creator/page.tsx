c37b822f5a9cee7d4ceb4370086069b169a41e30
abhilashdesigner <abhilashkumar.web@gmail.com>
2025-11-15 19:55:20 +0530

    Update page.tsx

--- src/app/meet-creator/page.tsx
+++ src/app/meet-creator/page.tsx
@@ -10,7 +10,15 @@ export default function MeetCreatorPage() {
   return (
     <main className="pt-0 overflow-x-hidden">
       <MeetCreator />
-
+<section className="w-full flex justify-center items-center">
+//         <Image
+//           src="/images/meet-creator.png"
+//           alt="Media"
+//           width={1200}
+//           height={738}
+//           className="w-full h-[570px] md:h-[738px] lg:h-full object-cover"
+//         />
+//       </section>
       Abhilash
       <Logos />
       <Contact />

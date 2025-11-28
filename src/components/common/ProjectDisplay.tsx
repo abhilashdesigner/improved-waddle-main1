fb0cf3d636b19f37e80bfe2e9623cd1709ef7935
abhilashdesigner <abhilashkumar.web@gmail.com>
2025-11-28 14:04:20 +0530

    Update ProjectDisplay.tsx

--- src/components/common/ProjectDisplay.tsx
+++ src/components/common/ProjectDisplay.tsx
@@ -1,7 +1,5 @@
 "use client";
 import Image from "next/image";
-export default function TransitionCanvas() {
-  return (
-    <canvas id="transition"></canvas>
-  );
+export default function Component() {
+  return <div>Hello</div>;
 }
